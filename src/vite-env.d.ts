24b6048806c0c3fdf1e492cd1600fdf13b37503a
Stefano Scozziero <s.scozziero@blcglobal.net>
2025-08-19 20:00:49 -0300

    tipado png

---
+++ src/vite-env.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="vite/client" />
+
+// Por si tu editor no toma los tipos de vite/client, declaramos explícitos:
+declare module '*.png' {
+  const src: string
+  export default src
+}
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+declare module '*.jpeg' {
+  const src: string
+  export default src
+}
+declare module '*.svg' {
+  const src: string
+  export default src
+}
+declare module '*.webp' {
+  const src: string
+  export default src
+}
+declare module '*.mp4' {
+  const src: string
+  export default src
+}
