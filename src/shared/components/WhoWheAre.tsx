2e5fe0721b109640a63ad80f92c160bb1db32e52
Stefano Scozziero <s.scozziero@blcglobal.net>
2025-08-21 22:16:10 -0300

    cadena de valor

--- src/shared/components/WhoWheAre.tsx
+++ src/shared/components/WhoWheAre.tsx
@@ -10,12 +10,12 @@ export function WhoWheAre() {
             QUIÉNES SOMOS:
           </h2>
 
-          <div className="mt-8 mx-auto max-w-3xl">
-            <p className="font-medium text-brand-blue/90 text-lg md:text-xl leading-8 text-center">
+          <div className="space-y-4 mt-8 mx-auto max-w-5xl">
+            <p className="font-medium text-brand-blue/90 text-lg md:text-xl leading-8 text-center md:text-left">
               Somos una empresa familiar con más de 100 años de trayectoria en la actividad de la carne, dedicada
               al engorde de animales de calidad superior, su faena y distribución en el mercado interno.
             </p>
-            <p className="mt-4 font-medium text-brand-blue/90 text-lg md:text-xl leading-8 text-center">
+            <p className="font-medium text-brand-blue/90 text-lg md:text-xl leading-8 text-center md:text-left">
               Nuestra historia está marcada por el compromiso con la calidad, la innovación en los procesos productivos
               y una fuerte vocación de servicio hacia nuestros clientes.
             </p>
